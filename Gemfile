0ad55343eddfcbcaf951c27bbe1b056e001d5543
Sebastian Marr <sbstnmrr@googlemail.com>
2012-12-29 14:34:23 +0100

    add MongoHQ compatibility

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem "data_mapper"
 gem "octokit"
 gem "mongo"
 gem "bson"
+gem "bson_ext"
 
 group :development do
   gem "thin"
--- index.rb
+++ index.rb
@@ -2,6 +2,7 @@ require "bundler"
 Bundler.require
 
 require "pp"
+require "uri"
 
 class Commit
 	include DataMapper::Resource
@@ -21,22 +22,30 @@ class Commit
 	end
 end
 
-mongo_grid = nil
+mongo_db = nil
+
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
 
 configure :development do
   DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  mongo_db = Mongo::MongoClient.new('localhost', 27017).db('lolcommits')
 end
 
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
+  mongo_db = get_connection
 end
 
-configure do
-  DataMapper.auto_upgrade!
-  mongo_db   = Mongo::MongoClient.new('localhost', 27017).db('lolcommits')
-  mongo_grid = Mongo::Grid.new(mongo_db)
-end
+DataMapper.auto_upgrade!
+mongo_grid = Mongo::Grid.new(mongo_db)
 
 get '/' do
 	@commits = Commit.all.reverse
